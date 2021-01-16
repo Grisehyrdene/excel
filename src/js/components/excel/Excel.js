d75e5faa869db0b0eb3060f3d627418afaf7fee0
Dmitri <Dmitri.Danshin2017@gmail.com>
2021-01-16 18:36:12 +0300

    dom modified

--- src/js/components/excel/Excel.js
+++ src/js/components/excel/Excel.js
@@ -2,7 +2,7 @@ import {$} from "../../core/dom";
 
 export class Excel{
     constructor(selector, options) {
-        this.$el = document.querySelector(selector);
+        this.$el = $(selector);
         this.components = options.components || [];
     }
 
@@ -13,12 +13,12 @@ export class Excel{
 
         this.components.forEach(Component =>{
            // в статических полях класса мы объявили название CSS класса
-           const $el = $.create('div', Component.className)
+           const $el = $.create('div', Component.className);
 
            // создаём экземпляр класса
            const component = new Component($el);
            // получаем его HTML
-           $el.innerHTML = component.toHTML();
+           $el.html(component.toHTML());
            // добавляем внутрь обёртки
            $root.append($el);
        });
--- src/js/core/dom.js
+++ src/js/core/dom.js
@@ -1,15 +1,62 @@
-class Dom{
+class Dom {
+    constructor(selector) {
 
+        // если передали сюда строку, которая означает селектор, который
+        // нам нужен, например #app или .button, то он при помощи этого селектора найдёт ноду в документе
+        // если передали непосредственное ноду, то он использует её.
+
+        this.$el = typeof selector === 'string'
+            ? document.querySelector(selector)
+            : selector;
+    }
+
+    // При передачи строки, метод записывает её в виде html.
+    html(html){
+        if(typeof html === 'string'){
+            this.$el.innerHTML = html;
+
+            // возврат this
+            // используется для того, чтобы выполнялся шаблон chain
+            // то есть можно создавать цепочку вызовов.
+            return this;
+        }
+        // иначе возвращает внешний html без пробелов (trim()).
+
+        return this.$el.outerHTML.trim();
+    }
+
+    clear(){
+        this.html('');
+        return this;
+    }
+
+    append(node){
+        // если node экземпляр класса
+        if (node instanceof Dom){
+            node = node.$el;
+        }
+
+        // создание полифила
+        if(Element.prototype.append){
+            this.$el.append(node);
+        }else{
+            this.$el.appendChild(node);
+        }
+        // возвращаем для поддержания цепочки
+        return this;
+    }
 }
 
-export function $(){
-    return new Dom();
+export function $(selector) {
+    return new Dom(selector);
 }
 
-$.create = (tagName, classes = '') =>{
+
+$.create = (tagName, classes = '') => {
     const el = document.createElement(tagName);
-    if (classes){
+    if (classes) {
         el.classList.add(classes);
     }
-    return el;
+    return $(el);
 }
+
